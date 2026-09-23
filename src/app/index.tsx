7bb77d3a89bc442c64f3a2bf6589f456169666e8
Yoval128 <yovalaviles812@gmail.com>
2026-09-23 11:05:21 -0600

    feat: configuracion de arranque

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,12 @@
-import { Text, View } from "react-native";
+import {Redirect} from 'expo-router';
 
-export default function Index() {
-    return (
-        <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-xl font-bold text-blue-500">
-                ¡Bienvenido a NativeWind con Expo!
-            </Text>
-            <Text className="text-xl font-bold text-red-500">
-                Prueba o NativeWind con Expo!
-            </Text>
-        </View>
-    );
+export default function IndexScreen() {
+    // Arranque de Encuestas
+    //return <Redirect href="/onboarding/step1" />;
+
+    // Arranque de Login
+    return <Redirect href="/(auth)/login"/>;
+    // Arranque de Register
+    // return <Redirect href="/(auth)/login" />;
+
 }
